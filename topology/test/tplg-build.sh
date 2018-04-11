6b955f0ed7f4b3212611e56c11cd0c9137f22088
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2018-04-11 15:00:13 -0500

    tplg: basic topologies for Reef support
    Need to work on DAI settings, not sure what the SSP config needs to be
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- topology/test/tplg-build.sh
+++ topology/test/tplg-build.sh
@@ -113,7 +113,10 @@ simple_test nocodec volume "NoCodec" s16le 4 s32le 32 32 3072000 24576000 I2S
 simple_test nocodec volume "NoCodec" s24le 4 s32le 32 32 3072000 24576000 I2S
 simple_test nocodec volume "NoCodec" s32le 4 s32le 32 32 3072000 24576000 I2S
 simple_test nocodec volume "NoCodec" s32le 4 s32le 32 32 3072000 24576000 LEFT_J
+simple_test codec volume "SSP1-Codec" s16le 1 s16le 16 16 1536000 24576000 I2S
 simple_test codec volume "SSP4-Codec" s16le 4 s16le 16 16 1536000 24576000 I2S
+simple_test codec volume "SSP5-Codec" s16le 5 s16le 16 16 1536000 24576000 I2S
+simple_test codec passthrough "SSP5-Codec" s16le 5 s16le 16 16 1536000 24576000 I2S
 simple_test nocodec volume "NoCodec" s16le 5 s16le 16 16 1536000 24576000 I2S
 simple_test nocodec src "NoCodec" s24le 4 s24le 32 24 3072000 24576000 I2S
 
